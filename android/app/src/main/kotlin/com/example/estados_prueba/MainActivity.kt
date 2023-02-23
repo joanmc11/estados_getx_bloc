8712ec16cb6308b59950f6c264e603bea3390d0e
Joan Marine <joan@jaleo.app>
2023-02-23 18:31:30 +0100

    App Gestion de estados

---
+++ android/app/src/main/kotlin/com/example/estados_prueba/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.estados_prueba
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/usuario_bloc.dart';
+//import 'pages/page_1.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => UsuarioBloc(),
+        )
+      ],
+      child: MaterialApp(
+        
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'pageApp',
+        routes: {
+          'pageApp': (_) => const PageApp(),
+        },
+      ),
+    );
+  }
+}
+
+class PageApp extends StatelessWidget {
+  const PageApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App Estados'),
+      ),
+    );
+  }
+}
